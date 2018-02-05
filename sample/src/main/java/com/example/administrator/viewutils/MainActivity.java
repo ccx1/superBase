5c21b8c1b67a129fd090294dc5ee9b144d1928f4
ci250454344 <ci250454344@163.com>
2018-02-05 15:48:00 +0800

    updata socket.io

--- sample/src/main/java/com/example/administrator/viewutils/MainActivity.java
+++ sample/src/main/java/com/example/administrator/viewutils/MainActivity.java
@@ -4,10 +4,20 @@ import android.widget.TextView;
 
 import com.example.administrator.superbase.view.NetworkStateView;
 
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 public class MainActivity extends BaseActivity {
 
 
     private NetworkStateView mNetworkStateView;
+    private Emitter.Listener connect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            System.out.println("链接成功");
+        }
+    };
+    private Socket mSocket;
 
     @Override
     protected int BindLayout() {
@@ -20,5 +30,18 @@ public class MainActivity extends BaseActivity {
         mNetworkStateView = (NetworkStateView) $(R.id.nwsv);
         registerNetWorkStateView(mNetworkStateView);
         TextView view = (TextView) $(R.id.textview);
+    }
+
+    @Override
+    protected void initData() {
+
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
     }
 }

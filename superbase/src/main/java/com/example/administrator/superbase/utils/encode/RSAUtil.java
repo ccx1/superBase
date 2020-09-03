1e9dfc2c552c3b3334ac2446b73d35a6748d09f6
ccx <ci250454344@163.com>
2020-09-03 09:40:46 +0800

    Update RSAUtil.java

--- superbase/src/main/java/com/example/administrator/superbase/utils/encode/RSAUtil.java
+++ superbase/src/main/java/com/example/administrator/superbase/utils/encode/RSAUtil.java
@@ -1,15 +1,10 @@
 package com.example.administrator.superbase.utils.encode;
 
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
+import javax.crypto.Cipher;
+import java.io.IOException;
+import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by v_chicunxiang on 2018/2/2.
@@ -20,73 +15,65 @@ import java.util.Map;
 
 public class RSAUtil {
 
-    public static final String KEY_ALGORITHM = "RSA";
-    public static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
-    public static final int KEY_SIZE = 2048;
-    public static final String PUBLIC_KEY = "PUBLIC_KEY";
-    public static final String PRIVATE_KEY = "PRIVATE_KEY";
-
-    public RSAUtil() {
+    //生成秘钥对
+    public static KeyPair getKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
     }
 
-    public static String sign(String sourceStr, String privateKeyStr, String charset) {
-        try {
-            byte[] sourceData = null;
-            if(charset != null && !"".equals(charset)) {
-                sourceData = sourceStr.getBytes(charset);
-            } else {
-                sourceData = sourceStr.getBytes();
-            }
-
-            byte[] privateKey = HexUtil.decodeHex(privateKeyStr.toCharArray());
-            PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(privateKey);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            PrivateKey priKey = keyFactory.generatePrivate(pkcs8KeySpec);
-            Signature signature = Signature.getInstance("SHA256withRSA");
-            signature.initSign(priKey);
-            signature.update(sourceData);
-            byte[] result = signature.sign();
-            return HexUtil.encodeHexString(result);
-        } catch (Exception var10) {
-            throw new RuntimeException(var10);
-        }
+    //获取公钥(Base64编码)
+    public static String getPublicKey(KeyPair keyPair){
+        PublicKey publicKey = keyPair.getPublic();
+        byte[] bytes = publicKey.getEncoded();
+        return byte2Base64(bytes);
     }
 
-    public static boolean verify(String sourceStr, String sign, String publicKey, String charset) {
-        try {
-            byte[] sourceData = null;
-            if(charset != null && !"".equals(charset)) {
-                sourceData = sourceStr.getBytes(charset);
-            } else {
-                sourceData = sourceStr.getBytes();
-            }
-
-            byte[] publicKeyBytes = HexUtil.decodeHex(publicKey.toCharArray());
-            byte[] signBytes = HexUtil.decodeHex(sign.toCharArray());
-            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            PublicKey pubKey = keyFactory.generatePublic(keySpec);
-            Signature signature = Signature.getInstance("SHA256withRSA");
-            signature.initVerify(pubKey);
-            signature.update(sourceData);
-            return signature.verify(signBytes);
-        } catch (Exception var11) {
-            throw new RuntimeException(var11);
-        }
+    //获取私钥(Base64编码)
+    public static String getPrivateKey(KeyPair keyPair){
+        PrivateKey privateKey = keyPair.getPrivate();
+        byte[] bytes = privateKey.getEncoded();
+        return byte2Base64(bytes);
     }
 
-    public static Map<String, String> generateKeyPair() {
-        HashMap keyMap = new HashMap();
+    //将Base64编码后的公钥转换成PublicKey对象
+    public static PublicKey string2PublicKey(String pubStr) throws Exception{
+        byte[] keyBytes = base642Byte(pubStr);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(keySpec);
+    }
 
-        try {
-            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
-            keyPairGen.initialize(2048);
-            KeyPair keyPair = keyPairGen.generateKeyPair();
-            keyMap.put("PUBLIC_KEY", HexUtil.encodeHexString(keyPair.getPublic().getEncoded()));
-            keyMap.put("PRIVATE_KEY", HexUtil.encodeHexString(keyPair.getPrivate().getEncoded()));
-            return keyMap;
-        } catch (Throwable var3) {
-            return keyMap;
-        }
+    //将Base64编码后的私钥转换成PrivateKey对象
+    public static PrivateKey string2PrivateKey(String priStr) throws Exception{
+        byte[] keyBytes = base642Byte(priStr);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+    //公钥加密
+    public static byte[] publicEncrypt(byte[] content, PublicKey publicKey) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(content);
+    }
+
+    //私钥解密
+    public static byte[] privateDecrypt(byte[] content, PrivateKey privateKey) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return cipher.doFinal(content);
+    }
+
+
+    //字节数组转Base64编码
+    public static String byte2Base64(byte[] bytes){
+        return Base64.encodeToString(bytes);
+    }
+
+    //Base64编码转字节数组
+    public static byte[] base642Byte(String base64Key) throws IOException {
+        return Base64.decode(base64Key);
     }
 }
